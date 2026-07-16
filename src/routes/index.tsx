aeea35cca87b85cbc72acd58b24d1267cf733a24
Derakhshan <github2reza@gmail.com>
2026-07-16 22:25:30 +0330

    feat(custome-desing): Configuration custome design... next step? create login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,14 @@
 import {
     BrowserRouter,
     Routes,
-    Route,
 } from "react-router-dom";
 
-import AuthLayout from "../layouts/AuthLayout";
-import DashboardLayout from "../layouts/DashboardLayout";
-
-import Login from "../pages/auth/Login";
 
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-
-                <Route path="/auth" element={<AuthLayout />}>
-                    <Route path="login" element={<Login />} />
-                </Route>
-
-                <Route path="/app" element={<DashboardLayout />}>
-                </Route>
-
             </Routes>
         </BrowserRouter>
     );
