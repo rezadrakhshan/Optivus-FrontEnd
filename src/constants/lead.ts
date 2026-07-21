4441504d7557349a913721b6857272fa3609f30b
Derakhshan <github2reza@gmail.com>
2026-07-21 10:29:27 +0330

    feat(map): Created 3 maps for leads type , tag and status

---
+++ src/constants/lead.ts
@@ -0,0 +1,34 @@
+export const leadTypeMap = {
+  THIRDPARTY: {
+    label: "شخص ثالث",
+    color: "blue",
+  },
+
+  CARBODY: {
+    label: "بدنه",
+    color: "violet",
+  },
+};
+
+export const leadStatusMap = {
+  WON: {
+    label: "برنده",
+    color: "green",
+  },
+
+  LOST: {
+    label: "از دست رفته",
+    color: "red",
+  },
+};
+
+export const leadTagMap = {
+  RENEWED: {
+    label: "تمدید",
+    color: "purple",
+  },
+  FOLLOWUP: {
+    label: "پیگیری",
+    color: "yellow",
+  },
+};
